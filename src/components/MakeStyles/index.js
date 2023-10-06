e76072c860a2b14a3f06a0c60ce8a95102400808
venu.bhupathiraju <venugopalvarma.bhupathiraju@cyient.com>
2023-10-06 23:04:38 +0530

    make styles

---
+++ src/components/MakeStyles/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { ColouredParagraph } from "./styledComponents";
+
+const MakeStyles = () => {
+  return <ColouredParagraph>MakeStyles</ColouredParagraph>;
+};
+
+export default MakeStyles;
---
+++ src/components/MakeStyles/styledComponents.js
@@ -0,0 +1,6 @@
+import styled from "styled-components/macro";
+
+export const ColouredParagraph = styled.p`
+  font-size: 44px;
+  color: red;
+`;
